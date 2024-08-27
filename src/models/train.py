839348b22a4a0021d156c9dc157781c8a1afce05
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-08-27 23:54:45 +0100

    updated: dataset func call

--- src/models/train.py
+++ src/models/train.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 from model import TripletLoss, SiameseModel, LSTM
 from src.data.dataset import MyDataset
 
-def train(num_epochs=10, input_shape=(250, 400)):
+def train(target_dir, num_epochs=10, input_shape=(250, 400)):
 
     
     lstm_network = LSTM(input_shape)
@@ -12,7 +12,7 @@ def train(num_epochs=10, input_shape=(250, 400)):
     criterion = TripletLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-    train_dataset = MyDataset(train=True)
+    train_dataset = MyDataset(target_loc=target_dir, data_type="train")
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     
     for epoch in range(num_epochs):
