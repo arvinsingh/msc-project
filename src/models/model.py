485b221b233113de23831b69e9c8b6fe4ed46711
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-08-30 22:20:39 +0100

    modified: added new func for model save, load & logging

--- src/models/model.py
+++ src/models/model.py
@@ -1,3 +1,4 @@
+import os
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
@@ -54,3 +55,58 @@ def euclidean_distance(vects):
     x, y = vects
     sum_square = torch.sum(torch.square(x - y), dim=1, keepdim=True)
     return torch.sqrt(torch.maximum(sum_square, torch.tensor(1e-07)))
+
+def setup_log_directory(training_config):
+    """Tensorboard Log and Model checkpoint directory Setup"""
+
+    if os.path.isdir(training_config.root_log_dir):
+        # Get all folders numbers in the root_log_dir.
+        folder_numbers = [int(folder.replace("version_", "")) for folder in os.listdir(training_config.root_log_dir)]
+
+        # Find the latest version number present in the log_dir
+        last_version_number = max(folder_numbers)
+
+        # New version name
+        version_name = f"version_{last_version_number + 1}"
+
+    else:
+        version_name = training_config.log_dir
+
+    # Update the training config default directory.
+    training_config.log_dir = os.path.join(training_config.root_log_dir, version_name)
+    training_config.checkpoint_dir = os.path.join(training_config.root_checkpoint_dir, version_name)
+
+    # Create new directory for saving new experiment version.
+    os.makedirs(training_config.log_dir, exist_ok=True)
+    os.makedirs(training_config.checkpoint_dir, exist_ok=True)
+
+    print(f"Logging at: {training_config.log_dir}")
+    print(f"Model Checkpoint at: {training_config.checkpoint_dir}")
+
+    return training_config, version_name
+
+def save_model(model, device, model_dir="models", model_file_name="AV_classifier.pt"):
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+
+    model_path = os.path.join(model_dir, model_file_name)
+
+    # Make sure you transfer the model to cpu.
+    if device == "cuda":
+        model.to("cpu")
+
+    # Save the 'state_dict'
+    torch.save(model.state_dict(), model_path)
+
+    if device == "cuda":
+        model.to("cuda")
+
+    return
+
+def load_model(model, model_dir="models", model_file_name="AV_classifier.pt", device=torch.device("cpu")):
+    model_path = os.path.join(model_dir, model_file_name)
+
+    # Load model parameters by using 'load_state_dict'.
+    model.load_state_dict(torch.load(model_path, map_location=device))
+
+    return model
