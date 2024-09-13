37d26d30a90443cdd875e9708ca69f7ca62241d3
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-09-13 23:19:39 +0100

    feat: archive folders

--- src/data/extract.py
+++ src/data/extract.py
@@ -1,14 +1,16 @@
 import os
+import shutil
 import py7zr
 
-# function recursively extracts all 7z files from a directory
-def extract_landmarks(data_path):
+
+def extract_landmarks(data_path='data\\raw\\dataset'):
     '''
     Extracts all 7z files from a directory and its subdirectories
     saves the extracted files in the same directory
     keep in separate folders where zip files are located
     :param data_path: path to the data directory
     '''
+
     for root, _, files in os.walk(data_path):
         for file in files:
             if file.endswith(".7z"):
@@ -18,6 +20,31 @@ def extract_landmarks(data_path):
                 os.remove(os.path.join(root, file))
 
 
-if __name__ == '__main__':
-    extract_landmarks('..\\data\\raw\\landmarks_classified')
+def archive_landmarks(data_path='data\\raw\\dataset'):
+    '''
+    Archives all extracted files in a directory and its subdirectories
+    :param data_path: path to the data directory
+    '''
 
+    for root, dirs, _ in os.walk(data_path):
+        for dir in dirs:
+            print(f'Archiving folders in {dir}...')
+            files = os.listdir(root + '\\' + dir)
+            for file in files:
+                if file.endswith((".wav", ".7z")):
+                    continue
+                filename = root + '\\' + dir + '\\' + file
+                with py7zr.SevenZipFile(filename + '.7z', mode='w') as archive:
+                    archive.writeall(filename, '')
+                shutil.rmtree(filename)
+
+if __name__ == '__main__':
+    choice = input("Enter '1' to extract landmarks, '2' to archive landmarks: ")
+    if choice == '1':
+        extract_landmarks()
+        print("Extraction complete")
+    elif choice == '2':
+        archive_landmarks()
+        print("Archiving complete")
+    else:
+        print("Invalid choice")
