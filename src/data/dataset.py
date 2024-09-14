5dd41bd63df47d3f4edc1500b21ceffc607fe4e7
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-09-14 22:48:02 +0100

    feat: dataset creation class

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,20 +1,44 @@
+import os
 import numpy as np
+import pandas as pd
+
+from src.features import mfcc
+from src.features import landmarks
+
 
 class MyDataset():
-    def __init__(self, target_loc, data_type):
-        if data_type == "train":
-            self.data = np.load(target_loc + "train_triplets.npy")
-            self.target = np.load(target_loc + "train_labels.npy")
-        elif data_type == "test":
-            self.data = np.load(target_loc + "test_triplets.npy")
-            self.target = np.load(target_loc + "test_labels.npy")
-        elif data_type == "val":
-            self.data = np.load(target_loc + "val_triplets.npy")
-            self.target = np.load(target_loc + "val_labels.npy")
 
-    def __len__(self):
-        return len(self.data)
 
-    def __getitem__(self, idx):
-        return self.data[idx], self.target[idx]
+    def __init__(self, location):
+        self.location = location
+        self.data_path = self.location + "dataset\\"
+        self.general_labels = pd.read_csv(self.location + "labels.csv", index_col=0)
+        self.labels = list()
+        self.data = list()
 
+
+    def create_dataset(self):
+        dirs = os.listdir(self.data_path)
+        for dir in dirs:
+            files = os.listdir(self.data_path + dir)
+            for i in range(1, 11):
+                # check if [1-10].wav file exists
+                if str(i) + ".wav" in files:
+                    # append label to labels list
+                    self.labels.append(self.general_labels.loc[dir][0])
+                    # generate MFCCs for .wav file
+                    frame = mfcc.process_audio_file(self.data_path + dir + "\\" + str(i) + ".wav")
+                    # open .7z file and extract all .ljson files to create a list of landmarks
+                    sequence = landmarks.get_mouth_sequences(self.data_path + dir + "\\" + str(i) + ".7z")
+                    # finally save
+                    self.data.append([frame, sequence])
+
+    
+    def save_dataset(self, save_path):
+        np.save(save_path + "data.npy", self.data)
+        np.save(save_path + "labels.npy", self.labels)
+    
+
+    def load_dataset(self, load_path):
+        self.data = np.load(load_path + "data.npy", allow_pickle=True)
+        self.labels = np.load(load_path + "labels.npy", allow_pickle=True)
