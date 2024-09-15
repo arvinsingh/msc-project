f5a7f3366d6150f874f22e2d07d0fc039fb19003
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-09-15 23:57:26 +0100

    feat: logging & debugging

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,9 +1,19 @@
 import os
 import numpy as np
 import pandas as pd
+import torch
 
 from src.features import mfcc
 from src.features import landmarks
+
+import logging
+
+# configure logging
+logging.basicConfig(
+    filename='dataset_creation.log',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 
 class MyDataset():
@@ -18,27 +28,35 @@ class MyDataset():
 
 
     def create_dataset(self):
+        _data = list()
         dirs = os.listdir(self.data_path)
         for dir in dirs:
             files = os.listdir(self.data_path + dir)
             for i in range(1, 11):
                 # check if [1-10].wav file exists
                 if str(i) + ".wav" in files:
-                    # append label to labels list
-                    self.labels.append(self.general_labels.loc[dir][0])
-                    # generate MFCCs for .wav file
-                    frame = mfcc.process_audio_file(self.data_path + dir + "\\" + str(i) + ".wav")
-                    # open .7z file and extract all .ljson files to create a list of landmarks
-                    sequence = landmarks.get_mouth_sequences(self.data_path + dir + "\\" + str(i) + ".7z")
-                    # finally save
-                    self.data.append([frame, sequence])
+                    try:
+                        # generate MFCCs for .wav file
+                        frame = mfcc.process_audio_file(self.data_path + dir + "\\" + str(i) + ".wav")
+                        frame = torch.squeeze(frame)
+                        # open .7z file and extract all .ljson files to create a list of landmarks
+                        sequence = landmarks.get_mouth_sequences(self.data_path + dir + "\\" + str(i) + ".7z")
+                        # finally save
+                        # append label to labels list
+                        _data.append([frame, sequence])
+                        self.labels.append(self.general_labels.loc[dir][0])
+                    except Exception as e:
+                        # log error if any
+                        logging.error(f"Error processing {dir}\\{i}: {e}")
+
+        self.data = _data
 
     
     def save_dataset(self, save_path):
-        np.save(save_path + "data.npy", self.data)
-        np.save(save_path + "labels.npy", self.labels)
+        torch.save(self.data, save_path + "data.pt")
+        torch.save(self.labels, save_path + "labels.pt")
     
 
     def load_dataset(self, load_path):
-        self.data = np.load(load_path + "data.npy", allow_pickle=True)
-        self.labels = np.load(load_path + "labels.npy", allow_pickle=True)
+        self.data = torch.load(load_path + "data.pt")
+        self.labels = torch.load(load_path + "labels.pt")
