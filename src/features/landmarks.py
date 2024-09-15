ac9792f905bde888a316b6e494b72f40108f8523
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-09-15 23:58:14 +0100

    fix: np array to torch tensor

--- src/features/landmarks.py
+++ src/features/landmarks.py
@@ -1,8 +1,10 @@
 import os
+import torch
 import numpy as np
 import py7zr
 import json
 import shutil
+
 
 def get_landmarks_dir(data_path):
     '''
@@ -18,17 +20,19 @@ def get_landmarks_dir(data_path):
                 dir_list.append(os.path.join(root, file))
     return dir_list
 
+
 def extract_mouth_contour_points(json_file):
-    mouth_contour_points = np.zeros((20, 3))
+    mouth_contour_points = torch.zeros((20, 3))
     with open(json_file, 'r') as f:
         data = json.load(f)
         landmarks = data['landmarks']['points']
         # points 48 to 68 correspond to the mouth contour
-        mouth_contour_points = landmarks[48: 68]
+        mouth_contour_points = torch.tensor(landmarks[48: 68])
     return mouth_contour_points
 
+
 def get_mouth_sequences(landmarks_path, max_frames=250):
-    sequences = np.zeros((3, max_frames, 20, 1))
+    sequences = torch.zeros((max_frames, 3, 20))
     with py7zr.SevenZipFile(landmarks_path, mode='r') as archive:
         archive.extractall('temp')
         for i, name in enumerate(archive.getnames()):
@@ -36,15 +40,17 @@ def get_mouth_sequences(landmarks_path, max_frames=250):
                 break
             if name.endswith(".ljson"):
                 data = extract_mouth_contour_points('temp\\' + name)
-                sequences[:, i, :, 0] = np.transpose(data)
+                sequences[i, :, :] = torch.transpose(data, 0, 1)
         shutil.rmtree('temp')
     return sequences
+
 
 def load_landmarks(dir_list, max_frames=150):
     landmarks = []
     for file in dir_list:
         landmarks.append(get_mouth_sequences(file, max_frames))
     return landmarks
+
 
 def create_landmarks_dataset(data_path, max_frames=150):
     dataset = []
@@ -54,8 +60,10 @@ def create_landmarks_dataset(data_path, max_frames=150):
     final_dataset = np.transpose(final_dataset, (4, 0, 1, 2, 3))
     return final_dataset
 
+
 def save_landmark_dataset(dataset, save_path, filename):
     np.save(save_path + filename, dataset)
+
 
 def load_landmark_dataset(data_path):
     return np.load(data_path)
