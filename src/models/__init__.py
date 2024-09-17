b59a7710f86ecb8adffd43422c1f7d2302a4b359
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-09-17 02:39:20 +0100

    fix: appropriate imports

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,19 +3,23 @@
 from .train import train
 from .validate import validate
 from .test import test
-from .model import LSTM, SiameseModel, euclidean_distance, save_model, load_model, MyModel
+from .model import LSTM, STGCN, SiameseModel, CombinedSiameseNetwork, euclidean_distance, save_model, load_model, audio_model, landmark_model, combined_model
 from .loss import triplet_loss
 from .main import main
 
 __all__ = [
     'main',
-    'MyModel',
+    'audio_model',
+    'landmark_model',
+    'combined_model',
     'train', 
     'validate', 
     'test', 
     'LSTM',
+    'STGCN',
     'triplet_loss', 
     'SiameseModel', 
+    'CombinedSiameseNetwork',
     'euclidean_distance',
     'save_model',
     'load_model',
