594717ef6dee6d12c184a5189474d1c4ee2edc70
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-09-20 21:31:38 +0100

    fix: comments, adjacency matrix correctly passed as arg

--- src/models/train.py
+++ src/models/train.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torchmetrics import MeanMetric
 from tqdm import tqdm
 
@@ -15,9 +14,10 @@ def train(
     train_loader: torch.utils.data.DataLoader,
     epoch_idx: int,
     total_epochs: int,
+    adj: torch.Tensor = None
 ) -> tuple[float, float]:
     
-    # Change model to training mode.
+    # change model to training mode.
     model.train()
 
     mean_metric = MeanMetric()
@@ -32,28 +32,28 @@ def train(
     prog_bar.set_description(status)
 
     for data in prog_bar:
-        # Send data to appropriate device.
+        # send data to appropriate device.
         anchor, positive, negative = data[0].to(device), data[1].to(device), data[2].to(device)
 
-        # Reset parameters gradient to zero.
+        # reset parameters gradient to zero.
         optimizer.zero_grad()
 
-        # Forward pass to the model.
-        positive_similarity, negative_similarity = model(anchor, positive, negative)
+        # forward pass to the model.
+        positive_similarity, negative_similarity = model(anchor, positive, negative, adj=adj)
 
-        # Triplet loss
+        # triplet loss
         loss = triplet_loss(positive_similarity, negative_similarity)
 
-        # Find gradients w.r.t training parameters.
+        # find gradients w.r.t training parameters.
         loss.backward()
 
-        # Update parameters using gradients.
+        # update parameters using gradients.
         optimizer.step()
 
-        # Batch Loss.
+        # batch Loss.
         mean_metric.update(loss.item(), weight=anchor.size(0))
 
-        # Apply threshold to determine correct predictions
+        # Aapply threshold to determine correct predictions
         threshold = train_config.threshold
         pos_correct = (positive_similarity < threshold).sum().item()
         neg_correct = (negative_similarity > threshold).sum().item()
@@ -61,7 +61,7 @@ def train(
         correct += pos_correct + neg_correct
         total += 2 * anchor.size(0)  # Two comparisons per triplet
 
-        # Update progress bar description.
+        # update progress bar description.
         step_status = status + f" Train Loss: {mean_metric.compute():.4f}, Train Acc: {correct / total:.4f}"
         prog_bar.set_description(step_status)
 
