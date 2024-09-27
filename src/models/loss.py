cf485218ddba3fb4c8a192827c769cb89ff44ec6
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-09-27 23:24:55 +0100

    feat: loss is now defined as class

--- src/models/loss.py
+++ src/models/loss.py
@@ -1,4 +1,7 @@
+import torch
+from torch import nn
 import torch.nn.functional as F
+
 
 def triplet_loss(positive_similarity, negative_similarity, margin=1.0):
     """
@@ -15,3 +18,15 @@ def triplet_loss(positive_similarity, negative_similarity, margin=1.0):
     loss = F.relu(positive_similarity - negative_similarity + margin)
     
     return loss.mean()
+
+
+class TripletLoss(nn.Module):
+    def __init__(self, margin=1.0):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, anchor, positive, negative):
+        positive_distance = F.pairwise_distance(anchor, positive, p=2)
+        negative_distance = F.pairwise_distance(anchor, negative, p=2)
+        loss = torch.relu(positive_distance - negative_distance + self.margin)
+        return loss.mean()
